468a7a153aaf9719e74beb3fd0eccf6826709a53
eduardomeloalbanese <eduardoalbaneze99@gmail.com>
2025-11-05 14:09:23 -0300

    fix: Garante que o Spring escaneia todos os Controllers

--- memorizasaude/src/main/java/br/com/fiap/memorizasaude/MemorizasaudeApplication.java
+++ memorizasaude/src/main/java/br/com/fiap/memorizasaude/MemorizasaudeApplication.java
@@ -2,8 +2,10 @@ package br.com.fiap.memorizasaude;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.fiap.memorizasaude")
 public class MemorizasaudeApplication {
 
 	public static void main(String[] args) {
